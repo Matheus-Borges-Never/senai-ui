7eef9698c7f07075e84e7ea42af669c7a3dec455
Matheus Borges <matheusborgespaulino@gmail.com>
2024-09-03 18:24:38 -0300

    feat: create component color secondary

--- senai-design-system/src/index.tsx
+++ senai-design-system/src/index.tsx
@@ -4,6 +4,9 @@ export const Hello = () => {
   return <div>Hello Wolrd!</div>;
 };
 
+// Colors
+
+// Primary
 export const primary50 = '#EEF7FF';
 export const primary100 = '#D8EDFF';
 export const primary200 = '#BAE0FF';
@@ -15,3 +18,16 @@ export const primary700 = '#0F59E6';
 export const primary800 = '#1348BA';
 export const primary900 = '#164194';
 export const primary950 = '#122859';
+
+// Secondary
+export const secondary50 = '##FFF6ED';
+export const secondary100 = '##FEEAD6';
+export const secondary200 = '#FDD1AB';
+export const secondary300 = '#FBB076';
+export const secondary400 = '#F8833F';
+export const secondary500 = '#F5631A';
+export const secondary600 = '#E84910';
+export const secondary700 = '#BF340F';
+export const secondary800 = '#982B14';
+export const secondary900 = '#7A2614';
+export const secondary950 = '#421008';
